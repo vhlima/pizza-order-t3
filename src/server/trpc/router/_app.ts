d65842f4bbad781a42e8d19b80913eed814d7052
vhlima <vhlima2000@gmail.com>
2022-11-18 06:20:43 -0300

    ⚰️ Remove example router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,16 +1,17 @@
 import { router } from '../trpc';
+
 import { drinkRouter } from './drink.routes';
 
-import { exampleRouter } from './example';
-
 import { pizzaRouter } from './pizza.routes';
+
+import { sideDishRouter } from './side-dish.routes';
 
 import { productCategoryRouter } from './product-category.routes';
 
 export const appRouter = router({
-  example: exampleRouter,
   pizza: pizzaRouter,
   drink: drinkRouter,
+  sideDish: sideDishRouter,
   productCategory: productCategoryRouter,
 });
 
